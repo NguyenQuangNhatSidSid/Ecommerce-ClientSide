e118b46452ff48dff3fd53219768ff997823bf2b
NguyenQuangNhatSidSid <derkaiserwind@gmail.com>
2024-09-18 16:58:59 +0700

    button components

--- components/Featured.js
+++ components/Featured.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Center from "./Center";
+import Button from "./Button";
 
 const Bg = styled.div`
   background-color: #222;
@@ -15,28 +16,56 @@ const Desc = styled.p`
   color: #aaa;
   font-size: 0.8rem;
 `;
-const Wrapper = styled.div`
+const ColumnsWrapper = styled.div`
   display: grid;
-  grid-template-columns: 0.8fr 1.2fr;
+  grid-template-columns: 0.9fr 1.1fr;
   gap: 40px;
   img {
     max-width: 100%;
   }
 `;
+
+const Column = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const ButtonsWrapper = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
 export default function Featured() {
   return (
     <Bg>
       <Center>
-        <Wrapper>
-          <div>
+        <ColumnsWrapper>
+          <Column>
             {" "}
-            <Title>Product anywhere</Title>
-            <Desc>Logo</Desc>
-          </div>
-          <div>
+            <div>
+              <Title>Product anywhere</Title>
+              <Desc>Logo</Desc>
+              <ButtonsWrapper>
+                <Button white outline size="l">
+                  Read more
+                </Button>
+                <Button primary size="l">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-6"
+                  >
+                    <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+                  </svg>
+                  Add to cart
+                </Button>
+              </ButtonsWrapper>
+            </div>
+          </Column>
+          <Column>
             <img src="https://firebasestorage.googleapis.com/v0/b/nextjs-ecommerce-7e047.appspot.com/o/images%2F1724487019844.jpg?alt=media&token=fc9295a2-eb0c-4f03-88c9-19cf8be4aa6a" />
-          </div>
-        </Wrapper>
+          </Column>
+        </ColumnsWrapper>
       </Center>
     </Bg>
   );
