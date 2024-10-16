65559e8e186d5a8f3932aa007c19530fc39f5409
NguyenQuangNhatSidSid <derkaiserwind@gmail.com>
2024-10-16 21:16:47 +0700

    remove cart after purchase

--- pages/cart.js
+++ pages/cart.js
@@ -49,7 +49,8 @@ const CityHolder = styled.div`
 `;
 
 export default function CartPage() {
-  const { cartProducts, addProduct, removeProduct } = useContext(CartContext);
+  const { cartProducts, addProduct, removeProduct, clearCart } =
+    useContext(CartContext);
   const [products, setProducts] = useState();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -57,6 +58,7 @@ export default function CartPage() {
   const [postalCode, setPostalCode] = useState("");
   const [streetAddress, setStreetAddress] = useState("");
   const [country, setCountry] = useState("");
+  const [isSuccess, setIsSuccess] = useState(false);
   useEffect(() => {
     if (cartProducts.length > 0) {
       axios
@@ -112,6 +114,17 @@ export default function CartPage() {
   const [total, setTotal] = useState(0);
 
   useEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+
+    if (window?.location.href.includes("success")) {
+      setIsSuccess(true);
+      clearCart();
+    }
+  }, [clearCart]);
+
+  useEffect(() => {
     if (Array.isArray(products) && cartProducts.length > 0) {
       let newTotal = 0;
       for (const productId of cartProducts) {
@@ -127,7 +140,7 @@ export default function CartPage() {
     }
   }, [products, cartProducts]);
 
-  if (window.location.href.includes("success")) {
+  if (isSuccess) {
     return (
       <>
         <Header />
